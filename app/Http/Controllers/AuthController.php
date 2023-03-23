6f5ac22c9da1d88ab3cded72531e03e5a641e876
Darren <58746026+Dazz563@users.noreply.github.com>
2023-03-23 14:18:31 +0200

    general sanctum implementation

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login(LoginUserRequest $req)
+    {
+        $req->validated($req->all());
+
+        if (!Auth::attempt($req->only('email', 'password'))) {
+            return $this->error('', 'Invalid email or password', 401);
+        }
+
+        $user = User::where('email', $req->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of ' . $user->name)->plainTextToken
+        ]);
+    }
+
+
+
+    public function register(StoreUserRequest $req)
+    {
+        $req->validated($req->all());
+
+        $user = User::create([
+            'name' => $req->name,
+            'email' => $req->email,
+            'password' => Hash::make($req->password),
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API token of ' . $user->name)->plainTextToken
+        ]);
+    }
+
+    public function logout()
+    {
+        return response()->json('This is my logout method');
+    }
+}
---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
+
+class TasksController extends Controller
+{
+    use HttpResponses;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user_id = Auth::user()->id;
+
+        $tasks = Task::where('user_id', $user_id)
+            ->orderBy('created_at', 'desc')
+            ->with('user')
+            ->get();
+
+        // return response()->json(['data' => $tasks]);
+        return $this->success($tasks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $request->validated($request->all());
+
+        $task = Task::create([
+            'user_id' => Auth::user()->id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'priority' => $request->priority,
+        ]);
+
+        return $this->success($task, 'Task created successfully', 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
